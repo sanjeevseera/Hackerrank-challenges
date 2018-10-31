8aab7093a666e29fbaf4cab8ccf0f864b0979f7a
sanjeevs <seerasanjeev@gmail.com>
2018-10-31 16:46:47 +0530

    MaxMin.py

--- MaxMin.py
+++ MaxMin.py
@@ -1,12 +1,19 @@
+"""
+You will be given a list of integers, arr, and a single integer k.
+You must create an array of length k from elements of arr such that its unfairness is minimized.
+Call that array subarr.
+Unfairness of an array is calculated as  max(subarr) - min(subarr)
+
+Where:
+- max denotes the largest integer in subarr
+- min denotes the smallest integer in subarr
+
+As an example, consider the array [1,4,7,2] with a k of 2. Pick any two elements, test subarr = [4,7].
+unfairness = max(4,7) - min(4,7) 7-4 = 3
+Testing for all pairs, the solution [1,2] provides the minimum unfairness.
+"""
+
 #!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
 # Complete the maxMin function below.
 def maxMin(k, arr):
     arr.sort()
