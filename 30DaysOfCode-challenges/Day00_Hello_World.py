ae442a20f709938efbecf64b750c41d1a6998afa
sanjeevs <seerasanjeev@gmail.com>
2018-11-26 15:03:13 +0530

    30DaysOfCode-challenges

---
+++ 30DaysOfCode-challenges/Day00_Hello_World.py
@@ -0,0 +1,13 @@
+"""
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World.
+on a single line, and finally print the value of your variable on a second line.
+"""
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
